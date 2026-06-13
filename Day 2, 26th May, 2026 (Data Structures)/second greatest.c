1cf35995fb2b9fffa585155fa6f8dd4439562b24
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:14 +0530

    Implement core logic in second greatest.c

--- Day 2, 26th May, 2026 (Data Structures)/second greatest.c
+++ Day 2, 26th May, 2026 (Data Structures)/second greatest.c
@@ -7,5 +7,14 @@ int main(){
     arr[0]=10;
     arr[1]=20;
     arr[2]=30;
+    arr[3]=40;
+    arr[4]=50;
+    greatest=arr[0];
+    for(i=1;i<5;i++){
+        if(arr[i]>greatest){
+            greatest=arr[i];
+        }
+    }
+    second_greatest=arr[0];
 
 }
