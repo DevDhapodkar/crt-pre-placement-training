b2376c215dc5e76dbaca988207106d7eb8818da1
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:12 +0530

    Implement core logic in if-else example.c

--- Day 1, 25th May, 2026 (Data Structures)/if-else example.c
+++ Day 1, 25th May, 2026 (Data Structures)/if-else example.c
@@ -3,5 +3,11 @@ int main(){
     int a;
     int b;
     printf("Enter the value of a and b: ");
+    scanf("%d %d", &a, &b);
+    if(a > b){
+        printf("a is greater than b");
+    }
+    else if(a < b){
+        printf("a is less than b");
 
-}
+}}
