d2a709eb1fbcaad15fcdd48bc27acf3af951c5bf
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:14 +0530

    Setup boilerplate and structure for check if palindrome.c

---
+++ Day 5, 29th May, 2026 (Data Structures)/check if palindrome.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; // Remove trailing newline character
+
+    int left = 0;
+
+}
