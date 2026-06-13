dbbb38cc2cb61ae535826a6908305a5015721b36
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:16 +0530

    Setup boilerplate and structure for arrayex1.java

---
+++ NagpurJavaProject/src/arrayex1.java
@@ -0,0 +1,8 @@
+public class arrayex1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int greatest = Integer.MIN_VALUE;
+        int secondGreatest = Integer.MIN_VALUE;
+
+
+}}
