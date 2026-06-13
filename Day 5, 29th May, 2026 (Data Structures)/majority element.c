f00ae27ca3d08dcfdfcdc75716fc00f514711edc
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:15 +0530

    Setup boilerplate and structure for majority element.c

---
+++ Day 5, 29th May, 2026 (Data Structures)/majority element.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+ int main() {
+    int arr[7] = {2, 2, 1, 1, 1, 2, 2};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int count = 0;
+    int candidate;
+     for (int i=0 ; i< size ; i++)
+    {
+        if(count == 0){
+            candidate = arr[i];
+
+}}}
