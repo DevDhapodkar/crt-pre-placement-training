de46939abf7802daf9bc1cef61830adcaaff6a1a
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:15 +0530

    Complete implementation and refactor linked lists op.c

--- Day 5, 29th May, 2026 (Data Structures)/linked lists op.c
+++ Day 5, 29th May, 2026 (Data Structures)/linked lists op.c
@@ -44,3 +44,24 @@ void insertAtPosition(struct Node** head, int data, int position) {
     newNode->next = temp->next;
     temp->next = newNode;
 }
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+int main() {
+    struct Node* head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 30);
+    insertAtBeginning(&head, 5);
+    insertAtPosition(&head, 15, 3);
+
+    printList(head);
+    return 0;
+}
