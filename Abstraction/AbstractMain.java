215b9a4cfa74363a2b7121d14c55a78ed5689003
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 12:04:57 +0530

    Setup abstract structure / definition for AbstractMain.java

---
+++ Abstraction/AbstractMain.java
@@ -0,0 +1,7 @@
+package Abstraction;
+// You cannot create obj of an abstract class but you can create reference variable of an abstract class
+public class AbstractMain {
+    public static void main(String[] args) {
+//dynamic method dispatch    
+
+}}
