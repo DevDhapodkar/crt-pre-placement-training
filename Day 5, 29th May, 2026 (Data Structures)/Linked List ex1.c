3ca46faa6362998f0735d3dea07b1b6c4a3251f7
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:14 +0530

    Implement core logic in Linked List ex1.c

--- Day 5, 29th May, 2026 (Data Structures)/Linked List ex1.c
+++ Day 5, 29th May, 2026 (Data Structures)/Linked List ex1.c
@@ -10,4 +10,16 @@ int main() {
     printf("address of a is %p \n", &a);
     printf("address of b is %p \n", &b);
 
+    int *ptr = &a;
+    float *ptr2 = &b;
+    int **pptr = &ptr;
+    int ***ppptr = &pptr;
+    int ****pppptr = &ppptr;
+    int *****ppppptr = &pppptr;
+    int ******pppppptr = &ppppptr;
+    printf("value of ptr is %p \n", ptr);
+    printf("Value of a using pointer is %d \n", *ptr);
+    printf("value of ptr2 is %p \n", ptr2);
+    printf("Value of b using pointer is %f \n", *ptr2);
+
 }
