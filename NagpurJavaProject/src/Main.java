97e5aea5364ed0ca702c47378627d2386a100e13
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:16 +0530

    Setup boilerplate and structure for Main.java

---
+++ NagpurJavaProject/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Nagpur";
+        String name = "Dev Dhapodkar";
+
+        System.out.println(str);
+        System.out.println(name);
+
+
+}}
