22c40769b0ae04d45a599b14ea142ec86416b8ff
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:13 +0530

    Implement core logic in tcs que.c

--- Day 1, 25th May, 2026 (Data Structures)/tcs que.c
+++ Day 1, 25th May, 2026 (Data Structures)/tcs que.c
@@ -2,5 +2,10 @@
 #include<stdio.h>
 int main(){
    int num1 = 7, num2 = 8, result;
+   
+    for(int i = 1; i<=10; i++){
+        result = num1 * i;
+        printf("%d ", result);
+        result = num2 * i;
 
-}
+}}
