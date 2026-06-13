00289040a4f2709f0fa975a1dde2b60defcaeb39
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:13 +0530

    Implement core logic in nested loop ex4.c

--- Day 1, 25th May, 2026 (Data Structures)/nested loop ex4.c
+++ Day 1, 25th May, 2026 (Data Structures)/nested loop ex4.c
@@ -5,5 +5,12 @@
 #include <stdio.h>
 int main(){
     int i, j, k = 1;
+    for (i = 1; i <= 3; i++){
+        for (j = 1; j <= 3; j++){
+            if (k % 2 != 0){
+                printf("%d ", k);
+            }
+            else{
+                printf("  ");
 
-}
+}}}}
