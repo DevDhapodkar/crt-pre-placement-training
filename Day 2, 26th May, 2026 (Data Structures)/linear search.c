5a83e896e99d040730a299e8205e034e67a9c7bc
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:14 +0530

    Implement core logic in linear search.c

--- Day 2, 26th May, 2026 (Data Structures)/linear search.c
+++ Day 2, 26th May, 2026 (Data Structures)/linear search.c
@@ -7,5 +7,14 @@ int main(){
     arr[0]=10;
     arr[1]=20;
     arr[2]=30;
+    arr[3]=40;
+    arr[4]=50;
 
-}
+    printf("Enter the element to be searched: ");
+    scanf("%d", &key);
+    for(i=0;i<max_size;i++){
+        if(arr[i]==key){
+            printf("Element found at index: %d", i);
+            return 0;
+
+}}}
