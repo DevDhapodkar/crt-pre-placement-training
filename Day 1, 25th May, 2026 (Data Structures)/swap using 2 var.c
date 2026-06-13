85e03e1e299ef7d686884c7c083c5cdfe7729658
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:13 +0530

    Implement core logic in swap using 2 var.c

--- Day 1, 25th May, 2026 (Data Structures)/swap using 2 var.c
+++ Day 1, 25th May, 2026 (Data Structures)/swap using 2 var.c
@@ -2,5 +2,9 @@
 
 int main()
 {
+    int a = 10, b = 20;
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    a = a + b; // a now becomes 30
+    b = a - b; // b now becomes 10 (30 - 20)
 
 }
