d56500f9c946c5877ea1978283f7a95ebe8b53c5
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:18 +0530

    Setup boilerplate and structure for Addition.java

---
+++ polymorphism1/Addition.java
@@ -0,0 +1,17 @@
+package polymorphism1;
+
+public class Addition {
+
+    public void add(){
+        System.out.println("Inside no parameter method");
+
+    int a = 10;
+    int b = 20;
+    int c = a+b;
+    System.out.println("Addition is : "+c);
+
+
+}
+public void add(int x){
+
+}}
