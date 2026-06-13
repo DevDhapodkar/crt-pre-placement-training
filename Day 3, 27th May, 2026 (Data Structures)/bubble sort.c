c5895a238f0d0331b93dd1669db29373ee47fe8f
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:14 +0530

    Implement core logic in bubble sort.c

--- Day 3, 27th May, 2026 (Data Structures)/bubble sort.c
+++ Day 3, 27th May, 2026 (Data Structures)/bubble sort.c
@@ -4,5 +4,12 @@ int main(){
     int size = sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<size-1;i++){
         for(int j=0;j<size-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 
-}}}
+}
