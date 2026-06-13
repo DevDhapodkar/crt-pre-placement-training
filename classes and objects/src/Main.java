9295039991b0d3a77397cf70e580d5d6a8e4059f
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:17 +0530

    Setup boilerplate and structure for Main.java

---
+++ classes and objects/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setStudent(101, "John", "Computer Science");
+        System.out.println("Student Details:");
+        s1.getStudent();
+
+        Student s2 = new Student();
+        s2.setStudent(102, "Alice", "Electrical Engineering");
+
+}}
