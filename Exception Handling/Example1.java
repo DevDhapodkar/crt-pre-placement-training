d732db898e325f23c56cb0ce41ee7576f8b2826c
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 12:12:23 +0530

    Add basic exception handling try-catch block to Example1.java

--- Exception Handling/Example1.java
+++ Exception Handling/Example1.java
@@ -3,5 +3,11 @@ public class Example1 {
     public static void main(String[] args) {
         try {
     int a = 10;
+    int b = 0;
+    int c = a/b;
+
+    System.out.println("Result: " + c);
+    System.out.println("End of the program");
+        } catch (ArithmeticException e) {
 
 }}}
