8aa14735c1974391211e396a653b526ba8adf8c1
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:14 +0530

    Setup boilerplate and structure for Linked List ex1.c

---
+++ Day 5, 29th May, 2026 (Data Structures)/Linked List ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 5.5;
+
+    printf("value of b is %f \n", b);
+    printf("value of a is %d \n", a);
+
+    printf("address of a is %p \n", &a);
+    printf("address of b is %p \n", &b);
+
+}
