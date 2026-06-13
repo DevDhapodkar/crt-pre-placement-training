f3997a7d61026adac63f10d0ebb5d3aef1ac995d
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:18 +0530

    Complete implementation and refactor B.java

--- polymorphism1/B.java
+++ polymorphism1/B.java
@@ -4,5 +4,6 @@ public class B extends A {
     public void show() {
 
         super.show(); // Calls the show method of class A
-
-}}
+        System.out.println("This is class B show method");
+    }
+}
