284b5b55f8dc403ac0574319d9d20bd32e7423b3
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:17 +0530

    Implement core logic in Main.java

--- polymorphism/src/Main.java
+++ polymorphism/src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Shape shape;
 
+        shape = new Circle();
+        shape.draw();
+
+        shape = new Rectangle();
+        shape.draw();
 
 }}
