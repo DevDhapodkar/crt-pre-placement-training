c535a7b5f8368ff3d2051d438171475b30d70679
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:16 +0530

    Setup boilerplate and structure for stringq1.java

---
+++ NagpurJavaProject/src/stringq1.java
@@ -0,0 +1,14 @@
+public class stringq1 {
+    public static void main(String[] args) {
+        String str = "madam";
+        int left = 0;
+        int right = str.length() - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+
+}}}
