f4ca72dffd7ef19c48d7ee52ec9cd8f31287371d
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:18 +0530

    Complete implementation and refactor methodOverridingMain1.java

--- polymorphism1/methodOverridingMain1.java
+++ polymorphism1/methodOverridingMain1.java
@@ -3,5 +3,6 @@ package polymorphism1;
 public class methodOverridingMain1 {
     public static void main(String[] args) {
         B b1 = new B();
-
-}}
+        b1.show(); // Calls method from class B
+    }
+}
