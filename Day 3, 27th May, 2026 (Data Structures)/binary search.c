2c0e4bb07b4fe6dada753c7f7d8929af01005168
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:14 +0530

    Implement core logic in binary search.c

--- Day 3, 27th May, 2026 (Data Structures)/binary search.c
+++ Day 3, 27th May, 2026 (Data Structures)/binary search.c
@@ -13,5 +13,20 @@ int binarySearch(int arr[], int low, int high, int key){
             high = mid - 1;
         }
     }
+    return -1;
+}
+int main(){
+    int arr[5];
+    int mid;
+    int low;
+    int high;
+    int key;
+    int max_size = sizeof(arr)/sizeof(arr[0]);
+    arr[0]=10;
+    arr[1]=20;
+    arr[2]=30;
+    arr[3]=40;
+    arr[4]=50;  
+    printf("Enter the element to be searched: ");
 
 }
