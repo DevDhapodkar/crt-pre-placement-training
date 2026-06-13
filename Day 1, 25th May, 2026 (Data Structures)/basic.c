35e26357d6a4572682a63751dfe572ac03f40e09
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:12 +0530

    Implement core logic in basic.c

--- Day 1, 25th May, 2026 (Data Structures)/basic.c
+++ Day 1, 25th May, 2026 (Data Structures)/basic.c
@@ -3,5 +3,10 @@ int main()
 {
     int a = 10;
     float f = 3.14;
+    char c = 'A';
+    printf(" Int = %d\n", a);
+    printf(" Float = %f\n", f);
+    printf(" Char = %c\n", c);
+    //these are the three primitive data types in C language whereas 
 
 }
