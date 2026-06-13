97a446d5c4ad77f9434ff87eb2db4a54150a3fa1
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:15 +0530

    Setup boilerplate and structure for linked lists op.c

---
+++ Day 5, 29th May, 2026 (Data Structures)/linked lists op.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
