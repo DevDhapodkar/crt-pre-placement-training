f07765150b19a857f8ae45f9dada6d54ed481874
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:16 +0530

    Implement core logic in structures.c

--- Day 5, 29th May, 2026 (Data Structures)/structures.c
+++ Day 5, 29th May, 2026 (Data Structures)/structures.c
@@ -7,5 +7,14 @@ struct Student {
         float grade;
     };
 int main(){
+    
+struct Student student1;
+student1.rollNumber = 100;
+student1.age = 20;
+student1.grade = 85.5;
+struct Student *ptr = &student1;
+
+printf("+-------------------------+\n");
+printf("|    Student 1 Details    |\n");
 
 }
