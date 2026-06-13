1c850cc5f413ae3203e1c891dd2c1d6e7af5fd7c
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:13 +0530

    Implement core logic in swap using 3 var.c

--- Day 1, 25th May, 2026 (Data Structures)/swap using 3 var.c
+++ Day 1, 25th May, 2026 (Data Structures)/swap using 3 var.c
@@ -2,5 +2,10 @@
 int main()
 {
     int a = 10; 
+    int b = 20;
+    int temp; // this variable is used to store the value of a or b temporarily while
+    // we are swapping the values of a and b
+    printf("Before swapping: a = %d , b = %d ",a,b);
+    temp = a;
 
 }
