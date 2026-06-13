96a6769db12fbe91f368598691d8f630c257f1ef
DevDhapodkar <dhapodkardev@icloud.com>
2026-06-13 11:24:13 +0530

    Implement core logic in table of 2.c

--- Day 1, 25th May, 2026 (Data Structures)/table of 2.c
+++ Day 1, 25th May, 2026 (Data Structures)/table of 2.c
@@ -2,5 +2,10 @@
 int main(){
     int num , result;
     printf("Enter a number: ");
+    scanf("%d", &num);
+    for(int i = 1; i<=10; i++){
+        result = num * i;
+        printf("%d x %d = %d\n", num, i, result);
+    }
 
 }
